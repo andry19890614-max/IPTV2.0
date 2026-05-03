f09a1577fd84ee9812d9ba3b3ec01f5802de60ed
andry19890614-max <andry19890614@gmail.com>
2026-05-03 14:15:24 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/iptv/player/MainActivity.java
@@ -0,0 +1,16 @@
+package com.iptv.player;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("IPTV Player works!");
+        setContentView(tv);
+    }
+}
